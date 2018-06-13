27dff6fa6b3b6560434e81a91d493e93d1ad6df3
udhos <everton.marques@gmail.com>
2018-06-13 19:09:57 -0300

    Azure skeleton.

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ go get gopkg.in/yaml.v2
 go get github.com/go-ini/ini
 go get github.com/jmespath/go-jmespath
 go get github.com/aws/aws-sdk-go-v2
+go get github.com/subosito/gotenv
+go get -d github.com/Azure/azure-sdk-for-go/...
 
 gofmt -s -w ./lake
 go tool fix ./lake
--- lake/azure.go
+++ lake/azure.go
@@ -2,10 +2,55 @@ package main
 
 import (
 	"fmt"
-	"log"
+	//"log"
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-05-01/network"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	//"github.com/Azure/go-autorest/autorest/to"
 )
 
 func cloudAzure(me, cmd string, args []string) error {
-	log.Printf("azure here")
-	return fmt.Errorf("%s: azure FIXME WRITEME", me)
+
+	switch cmd {
+	case "list":
+		return listAzure(me, cmd)
+	case "pull":
+		return fmt.Errorf("azure FIXME WRITEME: cmd=%s", cmd)
+	case "push":
+		return fmt.Errorf("azure FIXME WRITEME: cmd=%s", cmd)
+	}
+
+	return fmt.Errorf("unsupported azure command: %s", cmd)
+}
+
+func listAzure(me, cmd string) error {
+
+	nsgClient := network.NewVirtualNetworksClient("<subscriptionID>")
+	authorizer, errAuth := auth.NewAuthorizerFromEnvironment()
+
+	if errAuth != nil {
+		return errAuth
+	}
+
+	nsgClient.Authorizer = authorizer
+
+	it, errList := nsgClient.ListAllComplete(context.Background())
+	if errList != nil {
+		return errList
+	}
+
+	for ; it.NotDone(); it.Next() {
+		nsg := it.Value()
+		fmt.Printf("name=%s id=%s type=%s location=%s\n", unptr(nsg.Name), unptr(nsg.ID), unptr(nsg.Type), unptr(nsg.Location))
+	}
+
+	return nil
+}
+
+func unptr(p *string) string {
+	if p == nil {
+		return "<nil-string>"
+	}
+	return *p
 }
